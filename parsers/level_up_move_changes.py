93b002d7c4a24db63b0205d387532a2ad9146064
zhenga8533 <zhenga8533@gmail.com>
2025-01-14 21:16:39 -0500

    initialized level up move changes

--- parsers/level_up_move_changes.py
+++ parsers/level_up_move_changes.py
@@ -1,5 +1,38 @@
+from util.file import load, save
+import re
+import string
+
+
 def main():
-    pass
+    content = load("files/Level Up Move Changes.txt")
+
+    lines = content.split("\n")
+    n = len(lines)
+    md = ""
+    listing = False
+
+    for i in range(n):
+        line = lines[i].strip()
+
+        if line == "":
+            if listing:
+                md += "```\n\n"
+                listing = False
+            continue
+        elif line.startswith("#"):
+            md += f"**{line}**\n\n```\n"
+            listing = True
+        elif line.endswith("Pokémon"):
+            md += f"## {line}\n\n"
+        elif line == "Key" or line == "General Attack Changes":
+            md += f"## {line}\n\n```\n"
+            listing = True
+        else:
+            md += f"{line}\n"
+            if not listing:
+                md += "\n"
+
+    save("output/level_up_move_changes.md", md)
 
 
 if __name__ == "__main__":
