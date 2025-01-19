a5527e1e7a804c0f5fecdc3e596223dd0c725921
Allen Zheng <zhenga8533@gmail.com>
2025-01-19 17:09:11 -0500

    action replay code rework

---
+++ parsers/action_replay_codes.py
@@ -0,0 +1,82 @@
+from util.file import load, save
+
+
+def main():
+    content = load("files/Action Replay Codes.txt")
+
+    lines = content.split("\n")
+    n = len(lines)
+    md = ""
+
+    game_ids = {}
+    game_version = None
+
+    parse_code = False
+    codes = {}
+    code_data = {
+        "name": None,
+        "description": None,
+        "blaze_black": "",
+        "volt_white": "",
+    }
+
+    for i in range(n):
+        line = lines[i].strip()
+
+        if line == "":
+            code_name = code_data["name"]
+            if code_data["name"] is not None:
+                if code_data["name"] not in codes:
+                    codes[code_name] = code_data
+                else:
+                    codes[code_name]["blaze_black"] += code_data["blaze_black"]
+                    codes[code_name]["volt_white"] += code_data["volt_white"]
+
+                code_data = {
+                    "name": None,
+                    "description": None,
+                    "blaze_black": "",
+                    "volt_white": "",
+                }
+        elif line.startswith("Pokémon"):
+            game_version = "_".join(line.split(" ")[1:]).lower()
+            game_ids[game_version] = f"### {line}\n\n"
+            parse_code = True
+        elif line.startswith("Game ID (Full)"):
+            game_ids[game_version] += f"```\n{line}\n"
+        elif line.startswith("Game ID (Clean)"):
+            game_ids[game_version] += f"{line}\n```\n\n"
+        elif parse_code:
+            if code_data["name"] is None:
+                code_data["name"] = line
+            elif all(c.isupper() or c.isdigit() or c.isspace() for c in line):
+                code_data[game_version] += f"{line}<br>"
+            else:
+                code_data["description"] = line
+        else:
+            md += line + "\n\n"
+
+    md += "---\n\n## Game IDs\n\n"
+    md += game_ids["blaze_black"]
+    md += game_ids["volt_white"]
+    md += "---\n\n## Action Replay Codes\n\n"
+
+    for code in codes:
+        code_data = codes[code]
+        code_name = code_data["name"]
+        code_desc = code_data["description"]
+        code_bb = code_data["blaze_black"]
+        code_vw = code_data["volt_white"]
+
+        md += f"### {code_name}\n\n"
+        if code_desc is not None:
+            md += f"{code_desc}\n\n"
+        md += "| Blaze Black | Volt White |\n"
+        md += "|-------------|------------|\n"
+        md += f"| <pre>{code_bb}</pre> | <pre>{code_vw}</pre> |\n\n"
+
+    save("output/action_replay_codes.md", md)
+
+
+if __name__ == "__main__":
+    main()
